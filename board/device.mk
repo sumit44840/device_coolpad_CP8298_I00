a800f5d05fadcd9b632d5d653d69a7c0bab686ea
Unknown <sumitraichandani44840@gmail.com>
2017-06-16 23:39:49 +0530

    DEVICE TREE FOR COOLPAD NOTE 3 LITE
    Here is the device tree for coolpad note 3 lite based on latest Lineage 14.1

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := CP8298_I00,COOLPAD,k5fpr,CP8298_I00
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
